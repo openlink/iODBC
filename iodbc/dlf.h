233a9fab2bed51cf8d25a4469a677c9bd044d1f8
Patrick van Kleef <pkleef@openlinksw.com>
1998-12-27 12:15:37 +0000

    * Checkin of initial version from Ke Jin

---
+++ iodbc/dlf.h
@@ -0,0 +1,23 @@
+#ifndef	_DLF_H
+# define _DLF_H
+# include	<config.h>
+
+# ifdef DLDAPI_SVR4_DLFCN
+#    include	<dlfcn.h>
+# else
+     extern void FAR*	dlopen(char FAR* path, int mode);
+     extern void FAR*	dlsym(void FAR* hdll, char FAR* sym);
+     extern char FAR*	dlerror();
+     extern int		dlclose(void FAR* hdll);
+# endif
+
+# ifndef	RTLD_LAZY
+#    define	RTLD_LAZY       1
+# endif
+
+# define	DLL_OPEN(dll)		(void*)dlopen((char*)(path), RTLD_LAZY)
+# define	DLL_PROC(hdll, sym)	(void*)dlsym((void*)(hdll), (char*)sym)
+# define	DLL_ERROR()		(char*)dlerror()
+# define	DLL_CLOSE(hdll)		dlclose((void*)(hdll))
+
+#endif
---
+++ iodbc/main.c
@@ -0,0 +1,7 @@
+#include	<config.h>
+
+/* entry function used to build a share library on AIX  */
+int	__start()
+{
+	return 0;
+}
