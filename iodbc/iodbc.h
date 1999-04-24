717998099a77277557ed819eca711508078aee88
Patrick van Kleef <pkleef@openlinksw.com>
1999-04-24 09:47:47 +0000

    * Code cleanups (pvk)
    * Fixed errors in Windows macros (pvk)

--- iodbc/iodbc.h
+++ iodbc/iodbc.h
@@ -25,28 +25,28 @@
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 #ifndef	_CONFIG_H
-#define	_CONFIG_H
+#define _CONFIG_H
 
 #if	!defined(WINDOWS) && !defined(WIN32_SYSTEM)
-#define	_UNIX_
+#define _UNIX_
 
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 
-#define	MEM_ALLOC(size)	(malloc((size_t)(size)))
-#define	MEM_FREE(ptr)	{if(ptr) free(ptr);}
+#define MEM_ALLOC(size)	(malloc((size_t)(size)))
+#define MEM_FREE(ptr)	{if(ptr) free(ptr);}
 
-#define	STRCPY(t, s)	(strcpy((char*)(t), (char*)(s)))
-#define	STRNCPY(t,s,n)	(strncpy((char*)(t), (char*)(s), (size_t)(n)))
-#define	STRCAT(t, s)	(strcat((char*)(t), (char*)(s)))
-#define	STRNCAT(t,s,n)	(strncat((char*)(t), (char*)(s), (size_t)(n)))
-#define	STREQ(a, b)	(strcmp((char*)(a), (char*)(b)) == 0)
-#define	STRLEN(str)	((str)? strlen((char*)(str)):0)
+#define STRCPY(t, s)	(strcpy((char*)(t), (char*)(s)))
+#define STRNCPY(t,s,n)	(strncpy((char*)(t), (char*)(s), (size_t)(n)))
+#define STRCAT(t, s)	(strcat((char*)(t), (char*)(s)))
+#define STRNCAT(t,s,n)	(strncat((char*)(t), (char*)(s), (size_t)(n)))
+#define STREQ(a, b)	(strcmp((char*)(a), (char*)(b)) == 0)
+#define STRLEN(str)	((str)? strlen((char*)(str)):0)
 
-#define	EXPORT
-#define	CALLBACK
-#define	FAR
+#define EXPORT
+#define CALLBACK
+#define FAR
 
 typedef signed short SSHOR;
 typedef short WORD;
@@ -58,25 +58,29 @@ typedef int BOOL;
 #endif /* _UNIX_ */
 
 #if	defined(WINDOWS) || defined(WIN32_SYSTEM)
-#include	<windows.h>
-#include	<windowsx.h>
+#include <windows.h>
+#include <windowsx.h>
 
 #ifdef	_MSVC_
-#define	MEM_ALLOC(size)	(fmalloc((size_t)(size)))
-#define	MEM_FREE(ptr)	((ptr)? ffree((PTR)(ptr)):0))
-#define	STRCPY(t, s)	(fstrcpy((char FAR*)(t), (char FAR*)(s)))
-#define	STRNCPY(t,s,n)	(fstrncpy((char FAR*)(t), (char FAR*)(s), (size_t)(n)))
-#define	STRLEN(str)	((str)? fstrlen((char FAR*)(str)):0)
-#define	STREQ(a, b)	(fstrcmp((char FAR*)(a), (char FAR*)(b) == 0)
+#define MEM_ALLOC(size)	(fmalloc((size_t)(size)))
+#define MEM_FREE(ptr)	((ptr)? ffree((PTR)(ptr)):0)
+#define STRCPY(t, s)	(fstrcpy((char FAR*)(t), (char FAR*)(s)))
+#define STRNCPY(t,s,n)	(fstrncpy((char FAR*)(t), (char FAR*)(s), (size_t)(n)))
+#define STRLEN(str)	((str)? fstrlen((char FAR*)(str)):0)
+#define STREQ(a, b)	(fstrcmp((char FAR*)(a), (char FAR*)(b) == 0))
+#define STRCAT(t, s)	(strcat((char*)(t), (char*)(s)))
+#define STRNCAT(t,s,n)	(strncat((char*)(t), (char*)(s), (size_t)(n)))
 #endif
 
 #ifdef	_BORLAND_
-#define	MEM_ALLOC(size)	(farmalloc((unsigned long)(size))
-#define	MEM_FREE(ptr)	((ptr)? farfree((void far*)(ptr)):0)
-#define	STRCPY(t, s)	(_fstrcpy((char FAR*)(t), (char FAR*)(s)))
-#define	STRNCPY(t,s,n)	(_fstrncpy((char FAR*)(t), (char FAR*)(s), (size_t)(n)))
+#define MEM_ALLOC(size)	(farmalloc((unsigned long)(size)))
+#define MEM_FREE(ptr)	((ptr)? farfree((void far*)(ptr)):0)
+#define STRCPY(t, s)	(_fstrcpy((char FAR*)(t), (char FAR*)(s)))
+#define STRNCPY(t,s,n)	(_fstrncpy((char FAR*)(t), (char FAR*)(s), (size_t)(n)))
 #define STRLEN(str)     ((str)? _fstrlen((char FAR*)(str)):0)
-#define STREQ(a, b)     (_fstrcmp((char FAR*)(a), (char FAR*)(b) == 0)
+#define STREQ(a, b)     (_fstrcmp((char FAR*)(a), (char FAR*)(b) == 0))
+#define STRCAT(t, s)	(strcat((char*)(t), (char*)(s)))
+#define STRNCAT(t,s,n)	(strncat((char*)(t), (char*)(s), (size_t)(n)))
 #endif
 
 #endif /* WINDOWS */
@@ -96,10 +100,10 @@ typedef int BOOL;
 #define DLDAPI_VMS_IODBC	/* Use enhanced dlopen() */
 #endif
 
-#define	SYSERR		(-1)
+#define SYSERR		(-1)
 
 #ifndef	NULL
-#define	NULL		((void FAR*)0UL)
+#define NULL		((void FAR*)0UL)
 #endif
 
 #endif
