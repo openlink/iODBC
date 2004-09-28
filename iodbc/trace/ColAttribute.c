e61bd1661d1129d1f44ae940381d631854c297f0
Patrick van Kleef <pkleef@openlinksw.com>
2004-09-28 13:36:23 +0000

    * Added symbolic tracing for SQLColAttribute (pvk)

--- iodbc/trace/ColAttribute.c
+++ iodbc/trace/ColAttribute.c
@@ -144,6 +144,175 @@ _trace_colattr3_type (SQLUSMALLINT type)
   trace_emit ("\t\t%-15.15s   %d (%s)\n", "SQLUSMALLINT ", (int) type, ptr);
 }
 
+static void
+_trace_colattr3_data (
+  SQLUSMALLINT		  FieldIdentifier,
+  SQLPOINTER		  CharacterAttributePtr,
+  SQLSMALLINT		  BufferLength,
+  SQLSMALLINT		* StringLengthPtr,
+  SQLLEN		* NumericAttributePtr,
+  int			  w_mode,
+  int			  output)
+{
+  int type = 0;
+
+  switch (FieldIdentifier)
+    {
+    case SQL_DESC_BASE_COLUMN_NAME:
+    case SQL_DESC_BASE_TABLE_NAME:
+    case SQL_DESC_CATALOG_NAME:
+    case SQL_DESC_LABEL:
+    case SQL_DESC_LITERAL_PREFIX:
+    case SQL_DESC_LITERAL_SUFFIX:
+    case SQL_DESC_LOCAL_TYPE_NAME:
+    case SQL_DESC_NAME:
+    case SQL_DESC_SCHEMA_NAME:
+    case SQL_DESC_TYPE_NAME:
+    case SQL_DESC_TABLE_NAME:
+      type = 1;			/* string */
+      break;
+
+    case SQL_DESC_CONCISE_TYPE:
+    case SQL_DESC_COUNT:
+    case SQL_DESC_DISPLAY_SIZE:
+    case SQL_DESC_LENGTH:
+    case SQL_DESC_NUM_PREC_RADIX:
+    case SQL_DESC_OCTET_LENGTH:
+    case SQL_DESC_PRECISION:
+    case SQL_DESC_SCALE:
+    case SQL_DESC_TYPE:
+      type = 2;			/* integer */
+      break;
+
+    case SQL_DESC_AUTO_UNIQUE_VALUE:
+    case SQL_DESC_CASE_SENSITIVE:
+    case SQL_DESC_FIXED_PREC_SCALE:
+    case SQL_DESC_UNSIGNED:
+      type = 3;			/* boolean */
+      break;
+
+    case SQL_DESC_NULLABLE:
+      type = 4;
+      break;
+
+    case SQL_DESC_SEARCHABLE:
+      type = 5;
+      break;
+
+    case SQL_DESC_UNNAMED:
+      type = 6;
+      break;
+
+    case SQL_DESC_UPDATABLE:
+      type = 7;
+      break;
+
+    default:
+      type = 0;			/* unknown */
+      break;
+    }
+
+  /*
+   *  If we cannot determine the type or we are entering, just
+   *  emit the pointers
+   */
+  if (type == 0 || !output)
+    {
+      _trace_pointer (CharacterAttributePtr);
+      _trace_bufferlen ((SQLINTEGER) BufferLength);
+      _trace_smallint_p (StringLengthPtr, output);
+      _trace_len_p (NumericAttributePtr, output);
+      return;
+    }
+
+  if (type == 1)		/* string */
+    {
+      if (w_mode)
+	_trace_string_w (CharacterAttributePtr, BufferLength, StringLengthPtr,
+	    output);
+      else
+	_trace_string (CharacterAttributePtr, BufferLength, StringLengthPtr,
+	    output);
+      _trace_bufferlen ((SQLINTEGER) BufferLength);
+      _trace_smallint_p (StringLengthPtr, output);
+      _trace_len_p (NumericAttributePtr, 0);	/* just display pointer */
+    }
+  else				/* integer */
+    {
+      _trace_pointer (CharacterAttributePtr);
+      _trace_bufferlen ((SQLINTEGER) BufferLength);
+      _trace_smallint_p (StringLengthPtr, 0);	/* just display pointer */
+
+      if (!NumericAttributePtr)	/* null pointer */
+	trace_emit ("\t\t%-15.15s * 0x0\n", "SQLLEN");
+      else if (type == 3)	/* boolean */
+	{
+	  trace_emit ("\t\t%-15.15s * %p (%s)\n", "SQLLEN",
+	      NumericAttributePtr,
+	      *(SQLLEN *) NumericAttributePtr ? "SQL_TRUE" : "SQL_FALSE");
+	}
+      else if (type == 4)	/* nullable */
+	{
+	  char *ptr = "unknown nullable type";
+
+	  switch (*NumericAttributePtr)
+	    {
+	      _S (SQL_NULLABLE);
+	      _S (SQL_NULLABLE_UNKNOWN);
+	      _S (SQL_NO_NULLS);
+	    }
+	  trace_emit ("\t\t%-15.15s * %p (%s)\n", "SQLLEN",
+	      NumericAttributePtr, ptr);
+	}
+      else if (type == 5)	/* searchable */
+	{
+	  char *ptr = "unknown searchable type";
+
+	  switch (*NumericAttributePtr)
+	    {
+	      _S (SQL_PRED_NONE);
+	      _S (SQL_PRED_CHAR);
+	      _S (SQL_PRED_BASIC);
+	      _S (SQL_PRED_SEARCHABLE);
+	    }
+	  trace_emit ("\t\t%-15.15s * %p (%s)\n", "SQLLEN",
+	      NumericAttributePtr, ptr);
+	}
+      else if (type == 6)	/* named */
+	{
+	  char *ptr = "unknown named type";
+
+	  switch (*NumericAttributePtr)
+	    {
+	      _S (SQL_NAMED);
+	      _S (SQL_UNNAMED);
+	    }
+	  trace_emit ("\t\t%-15.15s * %p (%s)\n", "SQLLEN",
+	      NumericAttributePtr, ptr);
+	}
+      else if (type == 7)	/* updatable */
+	{
+	  char *ptr = "unknown ubdatable type";
+
+	  switch (*NumericAttributePtr)
+	    {
+	      _S (SQL_ATTR_READONLY);
+	      _S (SQL_ATTR_WRITE);
+	      _S (SQL_ATTR_READWRITE_UNKNOWN);
+	   
+	    }
+	  trace_emit ("\t\t%-15.15s * %p (%s)\n", "SQLLEN",
+	      NumericAttributePtr, ptr);
+	}
+      else			/* just a value */
+	{
+	  _trace_len_p (NumericAttributePtr, output);
+	}
+    }
+
+  return;
+}
+
 
 void
 trace_SQLColAttribute (int trace_leave, int retcode,
@@ -163,10 +332,9 @@ trace_SQLColAttribute (int trace_leave, int retcode,
   _trace_handle (SQL_HANDLE_STMT, StatementHandle);
   _trace_usmallint (ColumnNumber);
   _trace_colattr3_type (FieldIdentifier);
-  _trace_pointer (CharacterAttributePtr);	/* TODO */
-  _trace_bufferlen ((SQLINTEGER) BufferLength);
-  _trace_smallint_p (StringLengthPtr, TRACE_OUTPUT_SUCCESS);
-  _trace_len_p (NumericAttributePtr, TRACE_OUTPUT_SUCCESS);
+
+  _trace_colattr3_data (FieldIdentifier, CharacterAttributePtr, BufferLength,
+      StringLengthPtr, NumericAttributePtr, 0, TRACE_OUTPUT_SUCCESS);
 }
 
 
@@ -178,7 +346,8 @@ trace_SQLColAttributeW (int trace_leave, int retcode,
   SQLPOINTER		  CharacterAttributePtr,
   SQLSMALLINT		  BufferLength,
   SQLSMALLINT		* StringLengthPtr,
-  SQLLEN		* NumericAttributePtr)
+  SQLLEN		* NumericAttributePtr
+  )
 {
   /* Trace function */
   _trace_print_function (en_ColAttributeW, trace_leave, retcode);
@@ -187,10 +356,9 @@ trace_SQLColAttributeW (int trace_leave, int retcode,
   _trace_handle (SQL_HANDLE_STMT, StatementHandle);
   _trace_usmallint (ColumnNumber);
   _trace_colattr3_type (FieldIdentifier);
-  _trace_pointer (CharacterAttributePtr);
-  _trace_bufferlen ((SQLINTEGER) BufferLength);
-  _trace_smallint_p (StringLengthPtr, TRACE_OUTPUT_SUCCESS);
-  _trace_len_p (NumericAttributePtr, TRACE_OUTPUT_SUCCESS);
+
+  _trace_colattr3_data (FieldIdentifier, CharacterAttributePtr, BufferLength,
+      StringLengthPtr, NumericAttributePtr, 1, TRACE_OUTPUT_SUCCESS);
 }
 
 
