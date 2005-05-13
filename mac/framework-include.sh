dadafb9c38b706b3fba27afd8ed2697e9e93ac59
Patrick van Kleef <pkleef@openlinksw.com>
2005-05-13 09:39:37 +0000

    * Added support for sqlucode.h (pvk)

--- mac/framework-include.sh
+++ mac/framework-include.sh
@@ -68,11 +68,12 @@
 
 
 mkdir -p include
-for i in sql.h sqlext.h sqltypes.h odbcinst.h iodbcinst.h isql.h isqlext.h isqltypes.h iodbcunix.h iodbcext.h
+for i in sql.h sqlext.h sqltypes.h sqlucode.h odbcinst.h iodbcinst.h isql.h isqlext.h isqltypes.h iodbcunix.h iodbcext.h
 do
     sed -e "s/<sql\.h/<iODBC\/sql.h/" \
 	-e "s/<sqlext\.h/<iODBC\/sqlext.h/" \
 	-e "s/<sqltypes\.h/<iODBC\/sqltypes.h/" \
+	-e "s/<sqlucode\.h/<iODBC\/sqlucode.h/" \
 	-e "s/<iodbcunix\.h/<iODBC\/iodbcunix.h/" \
 	-e "s/<iodbcext\.h/<iODBC\/iodbcext.h/" \
 	-e "s/<odbcinst\.h/<IODBCinst\/odbcinst.h/" < ../include/$i > include/$i
