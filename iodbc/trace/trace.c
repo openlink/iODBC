4ee6bb184d1dd1422ee3565bb4214879798144cf
Patrick van Kleef <pkleef@openlinksw.com>
2005-10-08 15:13:03 +0000

    * Include config.h if present (pvk)

--- iodbc/trace/trace.c
+++ iodbc/trace/trace.c
@@ -69,6 +69,11 @@
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
