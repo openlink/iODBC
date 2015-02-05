09c08833f56fdeaf9057685b7201d50e47d8d59d
iODBC Maintainer <iodbc@openlinksw.com>
2015-02-05 15:38:51 +0100

    Updated version strings to 3.52.10 for upcoming release

--- include/iodbc.h
+++ include/iodbc.h
@@ -84,13 +84,13 @@
 #endif
 
 #ifndef VERSION
-#define VERSION		"3.52.9"
+#define VERSION		"3.52.10"
 #define MAJ_VERSION	"3"
 #define MIN_VERSION	"52"
 #endif
 
 #ifndef IODBC_BUILD
-#define IODBC_BUILD 8120326	/* 0001.0928 */
+#define IODBC_BUILD 10150204	/* 0001.0928 */
 #endif
 
 #if	!defined(WINDOWS) && !defined(WIN32_SYSTEM)
