617c358f46bdafe9823613cab2de384f3b268e3a
Patrick van Kleef <pkleef@openlinksw.com>
2019-12-06 12:49:21 +0100

    Fixed compiler warnings

--- samples/iodbctest.c
+++ samples/iodbctest.c
@@ -788,7 +788,6 @@ ODBC_Test ()
       else if (!TXTCMP (request, TEXT ("environment")))
 	{
 	  extern char **environ;
-	  int i;
 
 	  for (i = 0; environ[i]; i++)
 	    fprintf (stderr, "%03d: [%s]\n", i, environ[i]);
@@ -978,9 +977,9 @@ ODBC_Test ()
 	  while (1)
 	    {
 #if (ODBCVER < 0x0300)
-	      int sts = SQLFetch (hstmt);
+	      sts = SQLFetch (hstmt);
 #else
-	      int sts = SQLFetchScroll (hstmt, SQL_FETCH_NEXT, 1);
+	      sts = SQLFetchScroll (hstmt, SQL_FETCH_NEXT, 1);
 #endif
 
 	      if (sts == SQL_NO_DATA_FOUND)
