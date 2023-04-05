f2bfb2fedb262fab6844771c8c7edb330d0a85a0
Patrick van Kleef <pkleef@openlinksw.com>
2023-04-05 18:35:44 +0200

    Updated build timestamp

--- include/iodbc.h
+++ include/iodbc.h
@@ -92,7 +92,7 @@
 #endif
 
 #ifndef IODBC_BUILD
-#define IODBC_BUILD 15210705	/* 0001.0928 */
+#define IODBC_BUILD 15230405	/* 0001.0928 */
 #endif
 
 #if	!defined(WINDOWS) && !defined(WIN32_SYSTEM)
